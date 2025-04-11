61ed635b9c61b2ad8a686c2925197a944cb9ca87
Monu_2114 <monu.nanabala@gmail.com>
2025-04-11 23:53:24 +0530

    route

---
+++ doraemon/app/api/routes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+const app = express();
+const port = 3001;
+
+app.use(express.json());
+
+router.get("/gadgets", (req, res) => {
+  res.send("Hello World");
+});
